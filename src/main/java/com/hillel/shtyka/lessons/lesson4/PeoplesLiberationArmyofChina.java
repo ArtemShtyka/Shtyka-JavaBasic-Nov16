7cc5f1b626022a12410f1a34c5a270ade30be7b7
Shtyka Artem <artyomshtyka@gmail.com>
2022-12-04 13:31:40 +0200

    Wrong source file replaced

--- src/main/java/com/hillel/shtyka/lessons/lesson4/PeoplesLiberationArmyofChina.java
+++ src/main/java/com/hillel/shtyka/lessons/lesson4/PeoplesLiberationArmyofChina.java
@@ -1,9 +1,14 @@
 package com.hillel.shtyka.lessons.lesson4;
 
-public class PeoplesLiberationArmyofChina {
-    public static void main(String[] args) {
+public class PeoplesLiberationArmyofChina
+{
+    public static void main(String[] args)
+    {
         int[] dmgLi = {13, 24, 46},
-                dmgMing = {9, 35, 12};
+            dmgMing = {9, 35, 12};
+        int partySize = 860;
         double multiplier = 1.5;
+        System.out.println("Загальні показники атаки:\n династія Лі: " + partySize*(dmgLi[0] + dmgLi[1] + dmgLi[2])
+        + "\n Династія Мінь: " + (int)(partySize*multiplier*(dmgMing[0] + dmgMing[1] + dmgMing[2])));
     }
 }
