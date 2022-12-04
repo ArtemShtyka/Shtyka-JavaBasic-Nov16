c83351cfedb750eec7570027c2f88b2e5330dae2
Shtyka Artem <artyomshtyka@gmail.com>
2022-12-04 10:25:47 +0200

    Initial

---
+++ src/main/java/com/hillel/shtyka/lessons/lesson4/PeoplesLiberationArmyofChina.java
@@ -0,0 +1,9 @@
+package com.hillel.shtyka.lessons.lesson4;
+
+public class PeoplesLiberationArmyofChina {
+    public static void main(String[] args) {
+        int[] dmgLi = {13, 24, 46},
+                dmgMing = {9, 35, 12};
+        double multiplier = 1.5;
+    }
+}
