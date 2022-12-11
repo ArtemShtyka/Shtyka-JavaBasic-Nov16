fea78985b352eb5a1954aa2ad5f6fe3030719b27
Shtyka Artem <artyomshtyka@gmail.com>
2022-12-11 09:46:18 +0200

    output formatting

--- src/main/java/com/hillel/shtyka/lessons/homework_6/CounterStrike.java
+++ src/main/java/com/hillel/shtyka/lessons/homework_6/CounterStrike.java
@@ -3,37 +3,13 @@ package com.hillel.shtyka.lessons.homework_6;
 import java.util.Scanner;
 
 public class CounterStrike {
-
     public static void main(String[] args) {
-
-        /*System.out.println("Введіть назву першої команди:");
-        while(true) {
-            team1Label = userInput.nextLine();
-            if(team1Label.length() == 0) {
-                System.out.println("Назва команди має містити принаймні один символ.");
-            }
-            else break;
-        }
-        System.out.println("Введіть назву другої команди:");
-        while (true) {
-            team2Label = userInput.nextLine();
-            if (team2Label.length() == 0) {
-                System.out.println("Назва команди має містити принаймні один символ.");
-            }
-            else {
-                if(team2Label.equalsIgnoreCase(team1Label)) {
-                    System.out.println("Назви команд не мають повторюватись.");
-                }
-                else break;
-            }
-        }*/
-
         String[] teamName = new String[2];
         int[][] teamFrags = new int [2][5];
         Scanner userInput = new Scanner(System.in);
+        int[] result = new int[2];
         int i = 0,
-            j = 0,
-            result = 0;
+            j = 0;
         while(i < 2) {
             System.out.println("Введіть назву " + (i + 1) + "-ї команди:");
             teamName[i] = userInput.nextLine();
@@ -42,10 +18,11 @@ public class CounterStrike {
                 System.out.println("Введіть кількість фрагів для " + teamFrags[i].length + " гравців команди " +
                         teamName[i] + ", розділяючи аргументи пробілом (\" \")");
 
-                while (j < 5) {
+                while (j < teamFrags[i].length) {
                     String token = userInput.next();
                     if(isInt(token) && Integer.parseInt(token) >= 0) {
                         teamFrags[i][j] = Math.abs(Integer.parseInt(token));
+                        result[i] += teamFrags[i][j];
                         j += 1;
                     }
                     else {
@@ -54,11 +31,16 @@ public class CounterStrike {
                 }
                 j = 0;
                 i += 1;
+
                 userInput.nextLine();
             }
             else System.out.println("Назва команди має містити принаймні один символ та не повторюватись для іншої команди.");
         }
-        System.out.println("Перемогла команда ");
+
+        System.out.println("Перемогла команда " + (result[0] >= result[1] ? (teamName[0] + ", набравши " + result[0] + " балів") :
+                (teamName[1] + ", набравши " + result[1] + " балів")));
+        System.out.println("Середній показник балів для команд: \n" + teamName[0] + ": " + (float)result[0]/teamFrags[0].length +
+                "\n" + teamName[1] + ": " + (float)result[1]/teamFrags[1].length);
 
     }
     public static boolean isInt(String arg) {
